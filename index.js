4ef0a1031ac6ecf653048a023791e29db81770a7
szolarisz <thomas@nyirszikszi.hu>
2021-12-12 17:07:21 +0100

    Működőképes verzió

--- index.js
+++ index.js
@@ -2,58 +2,93 @@ const http = require('http');
 const fs = require('fs');
 const port = 4405;
 
-const server = http.createServer( (req,res) => {
-    switch(true) {
+const server = http.createServer((req, res) => {
+    //console.log(`${req.url} -> ${req.method}`);
+
+    switch (true) {
+
 
         case req.url === "/" && req.method === "GET":
             fs.readFile("./view/index.html", (err, data) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.setHeader('Content-Type', 'text/html');
                 res.writeHead(200);
                 res.end(data);
-            })    
-        break;
+            })
+            break;
 
         case req.url === "/bootstrap.css" && req.method === "GET":
             fs.readFile("./node_modules/bootstrap/dist/css/bootstrap.min.css", (err, data) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.setHeader('Content-Type', 'text/css');
                 res.writeHead(200);
                 res.end(data);
-            })    
-        break; 
-        
+            })
+            break;
+
         case req.url === "/xmass.css" && req.method === "GET":
             fs.readFile("./view/xmass.css", (err, data) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.setHeader('Content-Type', 'text/css');
                 res.writeHead(200);
                 res.end(data);
-            })    
-        break; 
+            })
+            break;
 
-        case req.url === "/xmassdata.json" && req.method === "GET":
+        case req.url === "/xmassdata" && req.method === "GET":
             fs.readFile("./data/xmassdata.json", (err, data) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.setHeader('Content-Type', 'application/json');
                 res.writeHead(200);
                 res.end(data);
-            })    
-        break;
+            })
+            break;
 
         case req.url === "/munkas.js" && req.method === "GET":
             fs.readFile("./public/munkas.js", (err, data) => {
-                if(err) throw err;
+                if (err) throw err;
                 res.setHeader('Content-Type', 'application/javascript');
                 res.writeHead(200);
                 res.end(data);
-            })    
-        break;
+            })
+            break;
 
-         default:   
+        case req.url === "/xmassdata" && req.method === "POST":
+            //console.log("Itt a felvétel helye:");
+            let adatom = '';
+            req.on('data', (chunk) => {
+                adatom += chunk.toString();
+            });
+            req.on('end', () => {
+                const ujFuggo = JSON.parse(adatom);
+
+
+
+                fs.readFile('./data/xmassdata.json', (err, data) => {
+                    let adatok = JSON.parse(data);
+                    adatok.push({
+                        "atmero": ujFuggo.atmero,
+                        "piros": ujFuggo.piros,
+                        "alakzat": ujFuggo.alakzat
+                    });
+                    fs.writeFile('./data/xmassdata.json', JSON.stringify(adatok), () => {
+                        res.end(JSON.stringify(adatok));
+                    })
+                })
+            })
+
+            break;
+
+        default:
+            res.end('404-es hiba');
     }
 });
 
 server.listen(port);
+
+function sanitizeString(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü_-\s\.,]/gim, "");
+    return str.trim();
+}
 
 
--- public/munkas.js
+++ public/munkas.js
@@ -1,17 +1,17 @@
 document.getElementById('diszlista').onclick = diszekListaja;
 
 async function diszekListaja() {
-    const response = await fetch("/xmassdata.json");
+    const response = await fetch("/xmassdata");
     const diszlista = await response.json();
 
     var diszHTML = "<h1>Az eddigi díszek listája:</h1>";
     diszHTML += `<table id="disztabla"><tr><th>Átmérő (mm)</th><th>Szín</th><th>Alakzat</th></tr>`;
     for (const egyDisz of diszlista) {
         var sorClass = "arany";
-        const terfogat= Math.round(4*Math.PI*Math.pow(egyDisz.atmero/2,3)/3*100)/100;
-        if (egyDisz.piros ==="igaz")
+        const terfogat = Math.round(4 * Math.PI * Math.pow(egyDisz.atmero / 2, 3) / 3 * 100) / 100;
+        if (egyDisz.piros === "igaz")
             sorClass = "piros";
-        diszHTML += `<tr class=${sorClass}><td>${egyDisz.atmero} (V=${terfogat})</td>
+        diszHTML += `<tr class=${sorClass}><td>${egyDisz.atmero} ( V=${terfogat}  mm<sup>3</sup>)</td>
     <td class=>${sorClass}</td>
     <td>${egyDisz.alakzat}</td>
     </tr>
@@ -24,11 +24,16 @@ async function diszekListaja() {
     document.getElementById("diszeredmeny").innerHTML = diszHTML;
 }
 
-document.getElementById("ujDisz").onsubmit = async function (event) {
+document.getElementById("xmassform").onsubmit = async function (event) {
+    console.log("Elkapva");
     event.preventDefault();
-    const atmero = event.target.elements.code.value;
-    const piros = event.target.elements.name.value;
-    const alakzat = event.target.element.alakzat.value;
+    const atmero = event.target.elements.meret.value;
+    const pirosErtek = event.target.elements.piros.checked;
+    const alakzat = event.target.elements.alakzat.value;
+
+    var piros = "igaz"
+    if( pirosErtek == true)
+    piros = "hamis";
 
     const res = await fetch("/xmassdata", {
         method: "POST",
@@ -43,7 +48,7 @@ document.getElementById("ujDisz").onsubmit = async function (event) {
     });
 
     if (res.ok) {
-        diszekListaja()();
+        diszekListaja();
     } else {
         alert("Server error");
     }
